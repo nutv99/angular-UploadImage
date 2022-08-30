9d651b3d13aacbb9474c7df6480e077784609180
nutv99 <nutv99@gmail.com>
2022-08-30 10:31:20 +0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, VERSION } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  file: File = null; // Variable to store file
+  loading: boolean = false; // Flag variable
+
+  constructor(private http: HttpClient) {}
+
+  onChange(event) {
+    this.file = event.target.files[0];
+  }
+  baseApiUrl = 'https://lovetoshopmall.com/dataservice/uploadImage.php';
+
+  onUpload() {
+    this.loading = !this.loading;
+    console.log(this.file);
+    const formData = new FormData();
+    // Store form name as "file" with file data
+    formData.append('file', this.file, this.file.name);
+    this.http.post(this.baseApiUrl, formData).subscribe((response) => {
+      console.log(response);
+    });
+    console.log(this.file);
+  }
+}
